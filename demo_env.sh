3d7409b43a71932aa4f796cb3df594449c13819d
sergiodfdez <sergiodfdez@gmail.com>
2018-01-08 15:37:25 +0100

    add change

--- demo_env.sh
+++ demo_env.sh
@@ -6,4 +6,5 @@ export PATTON_URI=http://localhost:8000;
 export SHARED_VOLUME_PATH=/tmp/deeptracy;
 export LOCAL_PRIVATE_KEY_FILE=/Users/sergio.fernandez/PycharmProjects/deeptracy/tests/acceptance/private_key/local_id_rsa;
 export LOG_LEVEL=DEBUG;
+export A=a;
 export BROKER_URI=redis://localhost:6379
