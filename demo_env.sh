cfdf9a9bee998bdae036c7681ad2eb2e5bcdaf52
sergiodfdez <sergiodfdez@gmail.com>
2018-01-08 15:11:20 +0100

    change posgress port to 5432

--- demo_env.sh
+++ demo_env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export DATABASE_URI=postgresql://postgres:postgres@localhost:5432/deeptracy;
+export DATABASE_URI=postgresql://postgres:postgres@localhost:5433/deeptracy;
 export PLUGINS_LOCATION=/Users/sergio.fernandez/PycharmProjects/deeptracy/plugins;
 export PYTHONUNBUFFERED=1;
 export PATTON_URI=http://localhost:8000;
